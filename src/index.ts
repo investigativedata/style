150a01f1a825607b3352dc8478b2e45243729e6d
Simon Wörpel <simon.woerpel@pm.me>
2024-03-01 20:42:54 +0100

    🧑‍💻 Add entrypoint

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import theme from "./theme";
+
+import Animation from "./components/Animation";
+import ArrowDown from "./components/ArrowDown";
+import Card from "./components/Card";
+import Drawer from "./components/Drawer";
+import Header from "./components/Header";
+import Logo from "./components/Logo";
+import OpenMoji from "./components/OpenMoji";
+import SectionHeader from "./components/SectionHeader";
+
+import Hero from "./sections/Hero";
+import MediaScreen from "./sections/MediaScreen";
+import Screen from "./sections/Screen";
+
+export {
+  theme,
+  Animation,
+  ArrowDown,
+  Card,
+  Drawer,
+  Header,
+  Logo,
+  OpenMoji,
+  SectionHeader,
+  Hero,
+  MediaScreen,
+  Screen,
+};
