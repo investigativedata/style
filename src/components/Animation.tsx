4e19acc3e97800176f920d274fd616f181505422
Simon Wörpel <simon.woerpel@pm.me>
2024-03-01 19:47:30 +0100

    💫 add Animation component

---
+++ src/components/Animation.tsx
@@ -0,0 +1,58 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useRive } from "@rive-app/react-canvas-lite";
+import Box from "@mui/joy/Box";
+
+interface IAnimation {
+  readonly width: number;
+  readonly height: number;
+  readonly src: string;
+}
+
+export default function Animation({
+  src,
+  width,
+  height,
+  children,
+}: React.PropsWithChildren<IAnimation>) {
+  const [ratio, setRatio] = useState<number>(0);
+  const { RiveComponent } = useRive({
+    src,
+    autoplay: true,
+  });
+  const ref = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const currentWidth = ref.current?.offsetWidth || 0;
+    if (currentWidth > 0) {
+      setRatio(currentWidth / width);
+    }
+  }, [ref.current]);
+  return (
+    <Box style={{ width: "100%", height: "auto", display: "block" }} ref={ref}>
+      <Box
+        style={{
+          width: width * ratio,
+          height: height * ratio,
+          position: "relative",
+        }}
+      >
+        <RiveComponent />
+        {children && (
+          <Box
+            style={{
+              width: width * ratio,
+              height: height * ratio,
+              position: "absolute",
+              top: 0,
+              left: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {children}
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+}
---
+++ stories/Animation.stories.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Animation from "../src/components/Animation";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+
+const meta = {
+  title: "Components/Animation",
+  component: Animation,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [
+    (Story) => (
+      <Box minWidth="600px">
+        <Story />
+      </Box>
+    ),
+  ],
+} satisfies Meta<typeof Animation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    src: "./animations/investigraph-01.riv",
+    height: 800,
+    width: 1436,
+  },
+};
+
+export const WithContent: Story = {
+  args: {
+    src: "./animations/investigraph-01.riv",
+    height: 800,
+    width: 1436,
+    children: (
+      <Typography level="h1" sx={{ textAlign: "center" }}>
+        Build and manage
+        <br />
+        your structured data
+        <br />
+        for investigative
+        <br />
+        research projects
+      </Typography>
+    ),
+  },
+};
