7bfcd234092c88aa31c860c479167a777f130c36
Simon Wörpel <simon.woerpel@pm.me>
2024-05-25 18:31:34 +0200

    🐛 Add AnimatedLogo to default exports

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ export { default as Card } from "./components/Card";
 export { default as Drawer } from "./components/Drawer";
 export { default as Header } from "./components/Header";
 export { default as Logo } from "./components/Logo";
+export { default as AnimatedLogo } from "./components/AnimatedLogo";
 export { default as Image } from "./components/Image";
 export { default as SectionHeader } from "./components/SectionHeader";
 
 export { default as Hero } from "./sections/Hero";
-export { default as MediaHero } from "./sections/MediaHero";
 export { default as MediaScreen } from "./sections/MediaScreen";
 export { default as Screen } from "./sections/Screen";
