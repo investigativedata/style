6e869eeeb1fd8078b5882653fc9c4a96867f83d1
Simon Wörpel <simon.woerpel@pm.me>
2024-05-27 01:24:30 +0200

    📝 Add List story

---
+++ stories/List.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+
+const ListItems = [1, 2, 3, 4, 5, 6].map((i) => (
+  <ListItem>{`I am list item ${i}`}</ListItem>
+));
+
+const meta = {
+  title: "List",
+  component: List,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: "500px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof List>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    marker: "disc",
+    children: ListItems,
+  },
+};
+
+export const OrderedList: Story = {
+  args: {
+    component: "ol",
+    marker: "decimal",
+    children: ListItems,
+  },
+};
