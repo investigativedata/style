86019dc7c83e05107737b056d8bf8a84d27e7016
Simon Wörpel <simon.woerpel@pm.me>
2024-05-27 01:22:18 +0200

    🚧 (components) Add alt prop to Image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,13 +1,14 @@
 import AspectRatio, { AspectRatioProps } from "@mui/joy/AspectRatio";
 
-interface IImageComponent extends AspectRatioProps {
+export interface IImage extends AspectRatioProps {
   readonly src: string;
+  readonly alt?: string;
 }
 
-export default function Image(props: IImageComponent) {
+export default function Image(props: IImage) {
   return (
     <AspectRatio {...props}>
-      <img src={props.src} />
+      <img src={props.src} alt={props.alt || ""} />
     </AspectRatio>
   );
 }
