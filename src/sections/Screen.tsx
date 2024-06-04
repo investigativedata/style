cee249a4c6972cab58a3655e6b37974685c0c0a3
Simon Wörpel <simon.woerpel@pm.me>
2024-06-04 10:11:00 +0200

    🚧 (components) Pass sx props to Screen

--- src/sections/Screen.tsx
+++ src/sections/Screen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SxProps } from "@mui/joy/styles/types";
 import Box from "@mui/joy/Box";
 import Container from "@mui/joy/Container";
 import { ColorPaletteProp } from "@mui/joy/styles";
@@ -8,12 +9,14 @@ export interface IScreen {
   readonly maxWidth?: "sm" | "md" | "lg" | "xl";
   readonly background?: ColorPaletteProp;
   readonly fullHeight?: boolean;
+  readonly sx?: SxProps;
 }
 
 export default function Screen({
   background = "neutral",
   maxWidth = "xl",
   fullHeight = true,
+  sx = {},
   children,
 }: React.PropsWithChildren<IScreen>) {
   return (
@@ -28,6 +31,7 @@ export default function Screen({
         minHeight: fullHeight ? "100vh" : "auto",
         alignItems: "center",
         alignContent: "center",
+        ...sx,
       }}
     >
       <Container maxWidth={maxWidth}>{children}</Container>
