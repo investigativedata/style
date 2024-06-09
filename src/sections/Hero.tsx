399de00a2c763e408bc027a74b5aad8717507d7b
Simon Wörpel <simon.woerpel@pm.me>
2024-06-09 14:57:52 +0200

    💄 Tweak hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -73,25 +73,15 @@ export default function Hero({
       padding="0"
     >
       <Grid
-        md={6}
+        xs={12}
+        md={5}
         p={0}
-        width={{
-          xs: "100%",
-          md: "calc(100% * 6 / var(--Grid-columns))",
-        }}
-        maxWidth={{ sm: "70%", md: "auto" }}
         order={{ sm: 1, md: mediaRight ? 2 : 1 }}
         justifyContent="center"
       >
         {Media}
       </Grid>
-      <Grid
-        md={6}
-        order={{ sm: 2, md: mediaRight ? 1 : 2 }}
-        p={0}
-        paddingLeft={{ sm: 0, md: mediaRight ? 0 : 4 }}
-        textAlign={{ xs: "center", md: "inherit" }}
-      >
+      <Grid xs={12} md={6} order={{ sm: 2, md: mediaRight ? 1 : 2 }} p={0}>
         {Content}
       </Grid>
     </Grid>
